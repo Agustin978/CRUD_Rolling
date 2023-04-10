505fbd0fd2fabf9e5b2883100b0c90a79e40d5f5
Agustin978 <loboagus10@gmail.com>
2023-04-10 10:55:56 -0300

    Agregados detalles de como utilizar y como realizar llamadas a funciones para archivos js con el type module en html

--- js/adminPelis.js
+++ js/adminPelis.js
@@ -1,3 +1,11 @@
 import Pelicula from "./clasePelicula.js"; //Si no tuviera el default tendria que usar -->import {Pelicula} from ..
 let peli1 = new Pelicula('El juicio', 'algo', 'https://pics.filmaffinity.com/el_juicio-391136016-large.jpg', 2023, '177', 'Argentina', '-');
-console.log(peli1);
+
+const btnEditar = document.querySelector('#btnEditar')
+btnEditar.addEventListener('click', creaPeli);
+
+//Al colocar el type module desde html para el archivo js, el html no podra llamar a funciones puesto que el archivo js se volvera privado, por lo tanto debo usar los event desde js
+function creaPeli() 
+{
+    console.log(peli1);
+}
