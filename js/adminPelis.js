87b208aed01e2ab9d748ff46bca29b2109764a06
Agustin978 <loboagus10@gmail.com>
2023-04-26 10:29:11 -0300

    Desarrollado un control mayor de los datos que se ingresan para la creacion de la pelicula. Y realizado el almacenado en el localstorage de un arreglo de peliculas ingresadas

--- js/adminPelis.js
+++ js/adminPelis.js
@@ -5,9 +5,10 @@ let peli1 = new Pelicula('El juicio', 'algo', 'https://pics.filmaffinity.com/el_
 //const btnEditar = document.querySelector('#btnEditar')
 //btnEditar.addEventListener('click', creaPeli);
 const btnAgregar = document.getElementById('btnAgregar');
-btnAgregar.addEventListener('click', nuevaPeli);
+btnAgregar.addEventListener('click', mostrarModalPeli);
 const formularioPelicula = document.getElementById('formularioPeliculas');
 formularioPelicula.addEventListener('submit', cargarPeli);
+
 
 //Datos de la pelicula
 const codigo = document.getElementById('inputCodigo');
@@ -27,7 +28,7 @@ const modalPeli = new bootstrap.Modal(document.getElementById('modalAgregar'));
 
 //Al colocar el type module desde html para el archivo js, el html no podra llamar a funciones puesto que el archivo js se volvera privado, por lo tanto debo usar los event desde js
 
-function nuevaPeli()
+function mostrarModalPeli()
 {   
     modalPeli.show();
 }
@@ -35,16 +36,35 @@ function nuevaPeli()
 function cargarPeli(e)
 {
     e.preventDefault();
-    console.log('Cargando pelicula...');
-    let resumen = sumarioValidaciones(titulo,descripcion,imagen);
+    let resumen = sumarioValidaciones(titulo.value, descripcion.value, imagen.value, duracion.value, genero.value, anio.value);
     if(resumen.length === 0)
     {
-        console.log('Correcto');
+        console.log('Cargando pelicula...');
+        let peliculaIngresa = new Pelicula(titulo.value, descripcion.value, imagen.value, genero.value, anio.value, duracion.value, pais.value, reparto.value);
+        listaPeliculas.push(peliculaIngresa);
+        guardaEnLocalStorage();
+        limpiaFormulario();
         modalPeli.hide();
     }else   
     {
-        msjFormulario.className = 'alert alert-danger';
+        msjFormulario.className = 'alert alert-danger mt-2';
         msjFormulario.innerHTML = resumen;
     }
     
+}
+
+function guardaEnLocalStorage()
+{
+    localStorage.setItem('listadoPeliculas',JSON.stringify(listaPeliculas));
+}
+
+function limpiaFormulario()
+{
+    formularioPelicula.reset();
+}
+
+function creaPeli()
+{
+    let nuevaPeli = new Pelicula('Super Mario', 'algo', 'url', 'aventura', 2023, '2hs', 'EEUU', '-');
+    console.log(nuevaPeli);
 }
